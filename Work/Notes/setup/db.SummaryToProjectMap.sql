1bb89bc48d0a39a4164d000973333fd662f05772
Bill Stidham <billstidham@gmail.com>
2018-03-06 16:30:47 -0500

    Update db.SummaryToProjectMap.sql

--- Work/Notes/setup/db.SummaryToProjectMap.sql
+++ Work/Notes/setup/db.SummaryToProjectMap.sql
@@ -1,2 +1,11 @@
+CREATE TABLE IF NOT EXISTS SummaryToProjectMap (
+      SummaryToProjectMapID INTEGER PRIMARY KEY AUTOINCREMENT
+    , SummaryMask TEXT NOT NULL UNIQUE
+    , Project TEXT NOT NULL
+    , ActivityStatusCode TEXT NOT NULL DEFAULT 'A'
+    , CreatedDate TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
+    , ModifiedDate TEXT
+);
+
 INSERT INTO SummaryToProjectMap(SummaryMask, Project) VALUES('% - SUMMARY MASK HERE - %', 'PROJECT HERE');
 REPEAT...
